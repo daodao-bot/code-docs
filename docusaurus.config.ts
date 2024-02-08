eeaebea0db1c6e7a4c8b87ab6027a4e4ccdaad02
DaoDao <625117069@qq.com>
2024-02-08 13:21:32 +0800

    add GitHub workflows

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -37,15 +37,13 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/daodao-bot/code-docs/tree/main/docs/',
+          editUrl: 'https://github.com/daodao-bot/code-docs/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-              'https://github.com/daodao-bot/code-docs/tree/main/blog/',
+          editUrl: 'https://github.com/daodao-bot/code-docs/tree/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -153,7 +151,14 @@ const config: Config = {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
     },
+    mermaid: {
+      // theme: {light: 'neutral', dark: 'forest'},
+    }
   } satisfies Preset.ThemeConfig,
+  markdown: {
+    mermaid: true,
+  },
+  themes: ['@docusaurus/theme-mermaid'],
 };
 
 export default config;
