0da24d8f7787864a763b0594fda3f67153423a7a
DaoDao <625117069@qq.com>
2024-03-06 11:13:29 +0800

    add github pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,8 @@ const config: Config = {
   url: 'https://daoodao.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  // baseUrl: '/',
+  baseUrl: '/code-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
